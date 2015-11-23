fa4fdc15681cb16855b17e2e8be1ee771e71ccc6
Mukosame <mukosame@gmail.com>
2015-11-23 18:58:00 +0800

    20151123

---
+++ ex37.py
@@ -0,0 +1,33 @@
+print "To remeber the operators and symbols"
+print "---------"
+print "Keywords"
+print "---------"
+
+
+print "---------"
+print "Data Type"
+print "---------"
+
+print "-----------------"
+print "Escape Sequences"
+print "-----------------"
+
+print"\ \ is how \\ much"
+print"\ ' is how \' much"
+print'\ " is how \" much'
+print"\ a is how \a much"
+print"\ b is how \b much"
+print"\ f is how \f much"
+print"\ n is how \n much"
+print"\ r is how \r much"
+print"\ t is how \t much"
+print"\ v is how \v much"
+
+print "---------------"
+print "String Formats"
+print "---------------"
+result = 5 ** 3
+print"**: " "5 ** 3 = ", result
+result = 5 // 3
+print"//: " "5 // 3 = ", result
+ 
